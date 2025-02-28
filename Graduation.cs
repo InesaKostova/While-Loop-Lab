2970bc99bf1a17f2b31177fbf3b419f4dd0c8c12
Inesa Kostova <kostovainesa@gmail.com>
2025-02-28 15:36:58 +0200

    Add files via upload

---
+++ Graduation.cs
@@ -0,0 +1,29 @@
+﻿string studentName = Console.ReadLine();
+
+int expelsCount = 0;
+int yearsCount = 0;
+double gradesSum = 0;
+while (expelsCount<2 && yearsCount<12)
+{
+    double grade = double.Parse(Console.ReadLine());
+    yearsCount++;
+    if(grade<4)
+    {
+        expelsCount++;
+        continue;
+    }
+    else
+    {
+        gradesSum+= grade;
+    }
+
+}
+
+if(expelsCount<2)
+{
+    Console.WriteLine($"{studentName} graduated. Average grade: {gradesSum/yearsCount:f2}");
+}
+else
+{
+    Console.WriteLine($"{studentName} has been excluded at {yearsCount-1} grade");
+}
