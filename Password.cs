bac4ae95e7df92e63f5cd1363025b52935c0ad95
Inesa Kostova <kostovainesa@gmail.com>
2025-02-28 14:23:20 +0200

    Add files via upload

---
+++ Password.cs
@@ -0,0 +1,10 @@
+﻿string username = Console.ReadLine();
+string password = Console.ReadLine();
+
+string currentpass =Console.ReadLine();
+
+while(currentpass!=password)
+{
+    currentpass = Console.ReadLine();
+}
+Console.WriteLine($"Welcome {username}!");
